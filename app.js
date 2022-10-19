aa89fcb95776bcfb5d95b64ce5fc15fcbee3d255
Henrique Vieira de Jesus <henriquerkn@gmail.com>
2022-10-18 21:05:11 -0300

    Programa para calculo de média

---
+++ app.js
@@ -0,0 +1,16 @@
+//  console.log("Olá, estou funcionando")
+//  console.log(10+3-5)
+//  console.log(9*5)
+//  console.log(112/10)
+//   console.log(1000000000000-100000000000)
+
+let nome = "yasha"
+let nota1 = 10
+let nota2 = 6
+let nota3 = 7
+let nota4 = 8
+let media = (nota1+nota2+nota3+nota4)/ 4
+let mediaFixada = media.toFixed(1)
+
+console.log("Ola "+nome)
+console.log("Sua Média foi: "+mediaFixada)
